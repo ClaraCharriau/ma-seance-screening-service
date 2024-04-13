57d5cb59960a9f5945476e226f006b155bcb1c8b
Clara Charriau <charriau.clara@gmail.com>
2024-04-13 10:34:29 +0200

    🚀 initial commit

---
+++ src/main/java/com/maseance/screeningservice/ScreeningServiceApplication.java
@@ -0,0 +1,13 @@
+package com.maseance.screeningservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreeningServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreeningServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/maseance/screeningservice/config/AppConfig.java
@@ -0,0 +1,20 @@
+package com.maseance.screeningservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
+
+@Configuration
+public class AppConfig {
+
+    /**
+     * Configuration that get the .env file to get environment variables
+     */
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+        configurer.setLocation(new FileSystemResource(".env.local"));
+        return configurer;
+    }
+}
