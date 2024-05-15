da11c31f4f9456d54d4fa20cfa430a6308601802
Clara Charriau <charriau.clara@gmail.com>
2024-05-15 14:09:34 +0200

    feat: add GET /theaters/search endpoint

--- src/main/java/com/maseance/screening/service/mapper/TheaterMapper.java
+++ src/main/java/com/maseance/screening/service/mapper/TheaterMapper.java
@@ -5,10 +5,14 @@ import com.maseance.screening.service.model.Theater;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+import java.util.List;
+
 @Mapper
 public interface TheaterMapper {
 
     TheaterMapper INSTANCE = Mappers.getMapper(TheaterMapper.class);
 
     TheaterDto toDto(Theater theaterEntity);
+
+    List<TheaterDto> toDtos(List<Theater> theaters);
 }
--- src/main/java/com/maseance/screening/service/repository/TheaterRepository.java
+++ src/main/java/com/maseance/screening/service/repository/TheaterRepository.java
@@ -4,8 +4,10 @@ import com.maseance.screening.service.model.Theater;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TheaterRepository extends JpaRepository<Theater, UUID> {
+    List<Theater> findByNameOrAddressContainingIgnoreCase(String name, String address);
 }
